9e6a776f7904b7da48b48c6c12a2190682d9a23b
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2023-02-04 10:54:53 -0500

    update the employee table with the role_id

--- db/my_db.js
+++ db/my_db.js
@@ -10,7 +10,7 @@ class MyDb {
     viewAllDepartments(){
         // show departments names and departments ids
         return new Promise((resolve, reject) => {
-            const sql = `SELECT id AS "Department Id", name AS "Department Name" FROM department`
+            const sql = `SELECT id AS "Department Id", name AS "Department Name" FROM department ORDER BY id`
             // return new Promise((resolve, reject) => {
             this.connection.query(sql, function(err, results){
                 if (err) {
@@ -30,7 +30,8 @@ class MyDb {
             SELECT role.id AS Role_ID, role.title AS Roles, department.name AS Departments, role.salary AS Salary
             FROM role
             JOIN department
-            ON department.id = role.department_id`
+            ON department.id = role.department_id
+            ORDER BY role.id;`
             // return new Promise((resolve, reject) => {
             this.connection.query(sql, function(err, results){
                 if (err) {
@@ -90,7 +91,7 @@ class MyDb {
         const selectSql = `SELECT id FROM department WHERE name = ?`;
         const insertSql = `
         INSERT INTO role (title, salary, department_id) 
-        VALUES (?, ?, ?);`;
+        VALUES (?, ?, ?);`
     
         return this.connection.promise().query(selectSql, [department_name])
             .then(([rows]) => {
@@ -169,7 +170,7 @@ class MyDb {
             return this.connection.promise().query(updateSql, [roleId, empFirstName, empLastName]);
         })
         .then(([rows]) => {
-            if (!rows.length) {
+            if (!rows.affectedRows) {
                 return Promise.reject(`No employee found with first name "${empFirstName}" or last name "${empLastName}"`);
             }
             console.log(`Employee "${employeeName}" has been updated with role "${newRole}"`);
